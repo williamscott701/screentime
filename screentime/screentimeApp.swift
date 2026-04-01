dfb59fc2760a4667768ebaf0a86fe32a681ac76b
w0p03c5 <williamscott.paka@walmart.com>
2026-04-01 07:37:47 +0530

    Initial Commit

---
+++ screentime/screentimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  screentimeApp.swift
+//  screentime
+//
+//  Created by Willam Scott on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct screentimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
